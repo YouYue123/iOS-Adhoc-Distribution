dbe063ce9e77ce4996352a6475b816cfb9b74341
youyue <youyue@sg.ibm.com>
2016-12-15 22:10:25 +0800

    [Feature]- configuration

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-'use strict';
+'use strict'
 const express = require('express')
 const fileSystem = require('fs')
 const path = require('path')
 const cfenv = require('cfenv')
+const ejs = require('ejs')
 const app = express()
-//set public as the static resource
+//set public as the static resource and view template
 app.use(express.static(__dirname + '/public'))
+app.set('views', __dirname + '/public');
+app.engine('html', require('ejs').renderFile);
 //set plist and ipa path
-const appPath = '/app/'+ process.env.npm_package_config_appName +'.ipa'
+const config = require('./config.js')
+const appPath = '/app/'+ config.APP_NAME +'.ipa'
 const plistFilePath = path.join(__dirname, '/app/manifest.plist')
 const ipaFilePath = path.join(__dirname, appPath)
+
+app.get('/',function(req,res){
+  const downloadUrl = "window.location.href='itms-services://?action=download-manifest&url="+config.WEB_URL + "/app/download'"
+  res.render('app.html', {downloadUrl: downloadUrl})
+})
+
 //response to download action
-app.get('/app/download_app',function(req,res){
+app.get('/app/download',function(req,res){
 	  res.set('Content-Type', 'text/xml plist')
 	  res.sendFile(plistFilePath)
 })
-//transfer app stream
+//Set HTTP Header and transfer app stream
 app.get(appPath,function(req,res){
 		res.set('Content-Type','application/octet-stream ipa')
 		res.sendFile(ipaFilePath)
@@ -25,5 +35,5 @@ var appEnv = cfenv.getAppEnv()
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
 	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+  console.log("server starting on " + appEnv.url)
 })
---
+++ config.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const config = {
+  APP_NAME: 'TestDistribution',
+  WEB_URL: 'https://sample-ios-distribution.au-syd.mybluemix.net'
+}
+module.exports = config
